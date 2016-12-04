90365ca2bc8aeb3eb0e432d1501481a5352cb13d
julianpascual <julianpascual96@gmail.com>
2016-12-04 05:19:58 -0300

    Home page trae todos los usuarios y los muestra en el mapa

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,11 +5,12 @@
         .module('tennisBuddiesApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', 'NgMap', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', 'NgMap', 'User' , '$state'];
 
-    function HomeController ($scope, Principal, LoginService, NgMap, $state) {
+    function HomeController ($scope, Principal, LoginService, NgMap, User, $state) {
         var vm = this;
         vm.account = null;
+        vm.users = [];
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
@@ -23,6 +24,8 @@
 
         getAccount();
 
+        getAllUsers();
+
         function getAccount() {
             Principal.identity().then(function(account) {
                 vm.account = account;
@@ -31,6 +34,31 @@
         }
         function register () {
             $state.go('register');
+        }
+
+        function getAllUsers () {
+            User.query(null, onSuccess, onError);
+        }
+
+        function onSuccess(data, headers) {
+            //hide anonymous user from user management: it's a required user for Spring Security
+            var hiddenUsersSize = 0;
+            for (var i in data) {
+                if (data[i]['login'] === 'anonymoususer') {
+                    data.splice(i, 1);
+                    hiddenUsersSize++;
+                }
+            }
+            vm.users = data;
+        }
+
+        function onError(error) {
+            console.log('Error retrieving users: ', error);
+        }
+
+        $scope.mySplit = function(string, nb) {
+            var array = string.split(';');
+            return array[nb];
         }
     }
 })();
